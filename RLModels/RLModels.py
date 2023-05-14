d02380dbbd7fa4f5b75107b812849fc3642d5463
liuyalinrich~ <tanguy.liuyalin@gmail.com>
2023-05-14 20:04:36 +0800

    try Rllib package..

--- RLModels/RLModels.py
+++ RLModels/RLModels.py
@@ -3,4 +3,4 @@ from stable_baselines3.common.evaluation import evaluate_policy
 from WindPowerSimulation.FlorisEnv import FlorisEnv
 env = FlorisEnv()
 #最后发现还是自己去写要更加简单容易一些。。
-# 可以尝试去使用一下 Rllibr
+# 可以尝试去使用一下 Rllibr(这个内容的效果好像还是可以的，原来软件有点卡的主要原因还是因为C盘有点太满了。。)
