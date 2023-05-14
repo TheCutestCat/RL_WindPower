59a82b38362c22ad02ee52f8468c4b54d5546e75
liuyalinrich~ <tanguy.liuyalin@gmail.com>
2023-05-14 09:28:38 +0800

    try to check the simulator and stable_baseline3

--- RLModels/RLModels.py
+++ RLModels/RLModels.py
@@ -1,7 +1,3 @@
-# use a simple model
-# 好像可以给出很多的信息:全局的风速图，当前每一台轮机的对用风速
-# 使用最简单的Q_learning，模型的选择范围很小
-# 而且当前我们尝试去使用具体的模型进行构建是非常有可能的。
-# 一个对那个的
-
-# 尝试一个influence_area 的位置，这样可以给出很多具体的结果
+from WindPowerSimulation.FlorisEnv import env
+import time
+myenv = env()
