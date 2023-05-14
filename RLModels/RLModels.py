6a6ad99df174a922fa30ac16dd11686ceeda9079
liuyalinrich~ <tanguy.liuyalin@gmail.com>
2023-05-14 14:24:15 +0800

    try different mdoel, but the result is not very good

--- RLModels/RLModels.py
+++ RLModels/RLModels.py
@@ -2,3 +2,5 @@ from stable_baselines3 import DQN
 from stable_baselines3.common.evaluation import evaluate_policy
 from WindPowerSimulation.FlorisEnv import FlorisEnv
 env = FlorisEnv()
+#最后发现还是自己去写要更加简单容易一些。。
+# 可以尝试去使用一下 Rllibr
